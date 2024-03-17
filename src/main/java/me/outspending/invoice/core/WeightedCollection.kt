d91e5fcd556279a97058e58772493efd25350835
Outspending <Dxrippy15@gmail.com>
2024-03-17 12:21:57 -0600

    Create WeightedCollection.kt

---
+++ src/main/java/me/outspending/invoice/core/WeightedCollection.kt
@@ -0,0 +1,57 @@
+package me.outspending.invoice.core
+
+import java.io.Serializable
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
+
+val random: ThreadLocalRandom = ThreadLocalRandom.current()
+
+class WeightedCollection<T> : Iterable<T>, Serializable {
+    private val items: NavigableMap<Double, T> = TreeMap()
+    private var totalWeight: Double = 0.0
+
+    fun add(weight: Double, item: T): WeightedCollection<T> {
+        if (weight <= 0) return this
+
+        totalWeight += weight
+        items[totalWeight] = item
+        return this
+    }
+
+    fun clear() {
+        items.clear()
+        totalWeight = 0.0
+    }
+
+    fun remove(item: T) {
+        val entry = items.entries.find { it.value == item } ?: return
+        items.remove(entry.key)
+        totalWeight -= entry.key
+    }
+
+    fun normalize() {
+        var currentWeight = 0.0
+        val newItems = TreeMap<Double, T>()
+        items.forEach { (weight, item) ->
+            currentWeight += weight
+            newItems[currentWeight] = item
+        }
+        items.clear()
+        items.putAll(newItems)
+        totalWeight = currentWeight
+    }
+
+    fun next(): T {
+        val value: Double = random.nextDouble() * totalWeight
+        return items.higherEntry(value).value
+    }
+
+    fun nextAndRemove(): T {
+        val next = next()
+        remove(next)
+
+        return next
+    }
+
+    override fun iterator(): Iterator<T> = items.values.iterator()
+}
